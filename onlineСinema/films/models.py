13a153eb98ad829a51351b6f8bcf01e1b4ff1da6
xeik <egor.kwakin@gmail.com>
2022-10-04 01:47:43 +0500

    description

--- onlineСinema/films/models.py
+++ onlineСinema/films/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import FileExtensionValidator
 
 # Create your models here.
 class Film(models.Model):
@@ -8,7 +9,8 @@ class Film(models.Model):
     film_year = models.CharField('Дата выхода фильма', max_length = 50)
     film_type = models.CharField('Тип', max_length = 50)
     description = models.TextField()
-
+    film_file = models.FileField(upload_to = 'video/' , validators = [FileExtensionValidator(allowed_extensions = ['mp4'])])
+    actors = models.TextField()
 
     def __str__(self):
         return self.film_title
