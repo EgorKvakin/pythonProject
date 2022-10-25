36b70a0a46bfc9bc52dd5e980fd339baf678dacf
admin <egor.kwakin@gmail.com>
2022-10-25 15:38:08 +0500

    qq

--- onlineСinema/films/admin.py
+++ onlineСinema/films/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
-from .models import Film, Actor, Categories
+from .models import Film, Actor, Categories, Series, SeriesVideo
 
 # Register your models here.
+class SeriesVideoAdmin(admin.ModelAdmin):
+  pass
+class SeriesVideoInline(admin.StackedInline):
+  model = SeriesVideo
+  max_num = 10
+  extra = 0
+
+
+class SeriesVideoAdmin(admin.ModelAdmin):
+  inlines = [SeriesVideoInline,]
+
 admin.site.register(Film)
 admin.site.register(Actor)
 admin.site.register(Categories)
+admin.site.register(Series,SeriesVideoAdmin)
--- onlineСinema/films/models.py
+++ onlineСinema/films/models.py
@@ -8,7 +8,6 @@ class Film(models.Model):
     film_title = models.CharField('Название фильма', max_length = 300)
     film_preview = models.ImageField('Превью фильма', upload_to = 'images/films')
     film_year = models.CharField('Дата выхода фильма', max_length = 50)
-    film_type = models.CharField('Тип', max_length = 50)
     description = models.TextField()
     film_file = models.FileField(upload_to = 'video/' , validators = [FileExtensionValidator(allowed_extensions = ['mp4'])])
     actor_film_list = models.ManyToManyField('Actor')
@@ -16,6 +15,22 @@ class Film(models.Model):
 
     def __str__(self):
         return self.film_title
+
+class Series(models.Model):
+    series_title = models.CharField('Название сериала', max_length = 300)
+    series_preview = models.ImageField('Превью сериала', upload_to = 'images/films')
+    series_year = models.CharField('Дата выхода сериала', max_length = 50)
+    description = models.TextField()
+    series_count = models.PositiveBigIntegerField('Кол-во серий')
+    seasons_count = models.PositiveIntegerField('Кол-во сезонов')
+    actor_film_list = models.ManyToManyField('Actor')
+    film_list = models.ManyToManyField('Categories')
+
+    def __str__(self):
+        return self.series_title
+class SeriesVideo(models.Model):
+  series_file = models.FileField(upload_to = f'video/series/{Series.series_title}' , validators = [FileExtensionValidator(allowed_extensions = ['mp4'])])
+  series = models.ForeignKey(Series,on_delete=models.CASCADE)
 
 class Actor(models.Model):
     name = models.CharField('Имя',max_length = 300)
